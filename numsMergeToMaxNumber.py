4dc5bb56f350265caf9b1457323ad1e35dc13fc3
qingfengwuhen <lingyu.666.happy@163.com>
2018-12-05 10:49:16 +0800

    Initial commit

---
+++ numsMergeToMaxNumber.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Zhao Baoxin on 12/5/18
+-------------------------------------------------
+   File Name：     numsMergeToMaxNumber
+   Description :
+   Author :       zhaobx
+   date：          12/5/18
+-------------------------------------------------
+   Change Activity:
+                   12/5/18:
+-------------------------------------------------
+"""
+import math
+def merge_to_maximal_number():
+    numLen = input()
+    nums = input()
+
+    
